4dac33c0691d88cb38cc3439c478136cbe577136
Aditya Agrawal <adityaagr00@gmail.com>
2022-06-09 08:27:38 +0530

    change webinar link

--- date.js
+++ date.js
@@ -16,12 +16,12 @@ for (let i = 0; i < regLinks.length; i++) {
   if (hours >= 11 && hours <= 20) {
     regLinks[i].setAttribute(
       "href",
-      "https://event.webinarjam.com/register/28/8q8lvbnp"
+      "https://event.webinarjam.com/register/70/ygo0ys73"
     );
   } else {
     regLinks[i].setAttribute(
       "href",
-      "https://event.webinarjam.com/register/28/8q8lvbnp"
+      "https://event.webinarjam.com/register/70/ygo0ys73"
     );
   }
 }
@@ -45,9 +45,9 @@ function redirect1() {
   // console.log(hours, minutes);
 
   if (hours >= 11 && hours <= 20) {
-    window.location.href = "https://event.webinarjam.com/register/28/8q8lvbnp";
+    window.location.href = "https://event.webinarjam.com/register/70/ygo0ys73";
   } else {
-    window.location.href = "https://event.webinarjam.com/register/28/8q8lvbnp";
+    window.location.href = "https://event.webinarjam.com/register/70/ygo0ys73";
   }
 }
 
