f837f2b4836b5faa2d88586e5c729c8dd95b42d1
Abhishek28112002 <pawanmittal2002@gmail.com>
2022-10-07 20:52:38 +0530

    scvfdv

--- date.js
+++ date.js
@@ -16,12 +16,12 @@ for (let i = 0; i < regLinks.length; i++) {
   if (hours >= 11 && hours <= 20) {
     regLinks[i].setAttribute(
       "href",
-      "https://event.webinarjam.com/register/76/8q8lvb1p"
+      "https://event.webinarjam.com/register/98/04klvh0g"
     );
   } else {
     regLinks[i].setAttribute(
       "href",
-      "https://event.webinarjam.com/register/76/8q8lvb1p"
+      "https://event.webinarjam.com/register/98/04klvh0g"
     );
   }
 }
@@ -45,9 +45,9 @@ function redirect1() {
   // console.log(hours, minutes);
 
   if (hours >= 11 && hours <= 20) {
-    window.location.href = "https://event.webinarjam.com/register/76/8q8lvb1p";
+    window.location.href = "https://event.webinarjam.com/register/98/04klvh0g";
   } else {
-    window.location.href = "https://event.webinarjam.com/register/76/8q8lvb1p";
+    window.location.href = "https://event.webinarjam.com/register/98/04klvh0g";
   }
 }
 
